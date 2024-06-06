71dcdb4df1e0bd96dc7393130d89b83b5501c09f
ellen-008 <ellen.marques.lara@escola.pr.gov.br>
2024-06-06 17:13:47 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,23 @@
+function somarNumeros(){
+  let numero1 =10;
+  let numero2=60;
+  let resultado = numero1 / numero2;
+  console.log(resultado);
+  texto = 'O resultado dessa divisão è: ' - resultado 
+  TextSize(25)
+  fill ("yellow")
+  text(texto, 5, 10, 350, 150);
+  
+}
+
+
+
+function setup() {
+  createCanvas(400, 400);
+  
+
+  
+  function draw() {
+  background(0)
+  dividirNumero();
+  }
